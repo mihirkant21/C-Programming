fbbc32c86b6fa059340eb5a837019aa54e08d50e
Mihirkant Pradhan <mihirkant.work@gmail.com>
2025-06-12 23:35:46 +0530

    Add files via upload
    Binary Search Tree in-pre-post order ( given)

---
+++ BST.C
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+    
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+    
+    return root;
+}
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+void preorder(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+void postorder(struct Node* root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+    int values[20] = {50, 30, 70, 20, 40, 60, 80, 10, 25, 35, 48, 55, 65, 75, 85, 5, 15, 33, 48, 90};
+
+    for (int i = 0; i < 20; i++) {
+        root = insert(root, values[i]);
+    }
+
+    printf("Inorder traversal: ");
+    inorder(root);
+    printf("\n");
+    
+    printf("Preorder traversal: ");
+    preorder(root);
+    printf("\n");
+    
+    printf("Postorder traversal: ");
+    postorder(root);
+    printf("\n");
+    
+    return 0;
+}
